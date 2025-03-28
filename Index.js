37cdb70a9eedb08af3a118e9c02bb293d24dbfbe
CharlesSoder <charles.soderstrom@skola.taby.se>
2025-03-28 12:05:34 +0100

    Påbörjan på Socket.io instruktioner (uppställning av sockets)

--- Index.js
+++ Index.js
@@ -1,1 +1,20 @@
-// testar en kommentar...
+// Har följt en socket tutorial och enligt socket.io instruktioner:
+// Nödvändiga funktioner
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+
+// Kopplar js sidan till html sidan (index.html)
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+// Servern körs på 3000
+server.listen(3000, () => {
+  console.log("listening on: 3000");
+});
