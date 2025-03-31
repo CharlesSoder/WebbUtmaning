f3b781c74aa4deedfbc4041a1f16439787f32390
CharlesSoder <charles.soderstrom@skola.taby.se>
2025-03-31 10:22:54 +0200

    Variabler för players "X" och "O" och påbörjan på en funktion att tilldela players "X" och "O" roller

--- Index.js
+++ Index.js
@@ -13,9 +13,22 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+let players = {}; // Sparar spelare som ansluter
+let board = Array(9).fill(null); // Tre i rad spelyta
+let currentTurn = "X"; // Börjar med att X alltid börjar spelet. Kommer ändra till varannan turn senare när spelet fungerar
+
 //cmd visar "a user" connectar och disconnectar
 io.on("connection", (socket) => {
   console.log("a user connected");
+  // Tilldela X eller O
+  if (!players.X) {
+    //Här ska en funktion för att ge en player id = spelare X
+  } else if (!players.O) {
+    //Här ska en funktion för att ge en player id = spelare O
+  } else {
+    socket.emit("full", "Spelet är fullt!");
+    return;
+  }
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
@@ -44,7 +57,7 @@ io.on("connection", (socket) => {
   socket.broadcast.emit("hi");
 });
 
-// Servern körs på 3000 och i mitt fall: http://192.168.56.1:3000
+// Servern körs på 3000 och i mitt fall: http://10.32.35.51:3000
 server.listen(3000, "0.0.0.0", () => {
   console.log("Listening on: http://0.0.0.0:3000");
 });
